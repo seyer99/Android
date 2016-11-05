b0f11373e66ea7f89f08912c456f1a54b8b5dd03
seyer99 <seyer.sawez@gmail.com>
2016-11-05 23:08:43 +0000

    New android code

---
+++ Test/app/src/main/java/net/techtinkers/test/Data.java
@@ -0,0 +1,13 @@
+package net.techtinkers.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}
